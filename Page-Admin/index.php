3cd58818bde5b21f70d62e9f93b0761ffe055fbe
UsmeGallego101206 <jfusme@outlook.es>
2023-03-21 10:25:43 -0500

    form

--- Page-Admin/index.php
+++ Page-Admin/index.php
@@ -67,6 +67,24 @@ if(!isset($_SESSION['correo'])){
         <h1>Administrador</h1>
 
     </section>
+    --proveedores--
+    <section>
+        <form action="productos.php" method="post">
+            <input type="text" name="marca">
+            <input type="text" name="modelo">
+            <input type="number" name="precio_base">
+            <input type="number" name="precio_descuento">
+            <input type="number" name="existencias">
+            <input type="text" name="disponibilidad">
+            <input type="text" name="descripcion">
+            <input type="number" name="Proveedor">
+        </form>
+    </section>
+    
+    --productos--
+    <section>
+        <form action="pro" method="post"></form>
+    </section>
     <!--=================== SwiperJS  ====================-->
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
 

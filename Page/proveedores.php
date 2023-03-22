223993b9915107cbd46070224268f17a62897d54
UsmeGallego101206 <jfusme@outlook.es>
2023-03-22 15:16:28 -0500

    Update proveedores.php

--- Page/proveedores.php
+++ Page/proveedores.php
@@ -16,3 +16,22 @@ echo"entró al guardar";
     }
 }
 ?>
+//clientes
+<?php
+include 'Conexion.php';
+echo"entró al guardar";
+    $cedula=$_POST['Cedula'];
+    $Nombre=$_POST['Nombre'];
+    $Apellido=$_POST['Apellido'];
+    $Correo=$_POST['Correo'];
+    $Telefono=$_POST['Telefono'];
+    if (isset($_POST['sub'])) {
+        echo"entró";
+
+    $sql = "INSERT INTO clientes (cedula, nombre, apellido, correo, Telefono) VALUES ('$cedula','$Nombre','$Apellido','$Correo','$Telefono')";
+    $rs = mysqli_query($con,$sql);
+    if ($rs) {
+        header('location:Index.php');
+    }
+}
+?>

6012b050725bae7666e31ae4146cb88cac9d8828
SkyBuilders32 <skybuilders32@gmail.com>
2023-03-25 22:32:55 -0500

    aa

--- Page/Admin/productos.php
+++ Page/Admin/productos.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['correo'])) {
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"  />
     <title>Megacom</title>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />

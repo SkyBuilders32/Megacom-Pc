d97a1b28d180962baa45ff15e4f7e2c310e5492c
UsmeGallego101206 <jfusme@outlook.es>
2023-05-22 21:03:34 -0500

    a

--- dev/page/admin/clientes/eliminar-editar.php
+++ dev/page/admin/clientes/eliminar-editar.php
@@ -43,11 +43,10 @@
   </div>
 </div>
 
-<div class="modal fade" id="edit_<?php echo $mostrar['Cedula']; ?>" tabindex="-1" aria-labelledby="ModalLabel"
-  aria-hidden="true">
+<div class="modal fade" id="edit_<?php echo $mostrar['Cedula']; ?>" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header">
+      <div class="modal-header bg-primary text-white">
         <h5 class="modal-title" id="ModalLabel">Editar Cliente</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
--- dev/page/admin/clientes/nuevo.php
+++ dev/page/admin/clientes/nuevo.php
@@ -8,8 +8,9 @@
             $nombre = $_POST['nombre'];
             $apellido = $_POST['apellido'];
             $correo = $_POST['correo'];
+            $telefono = $_POST['telefono'];
  
-            $sql = "INSERT INTO clientes (cedula, nombre, apellido, correo) VALUES ('$cedula','$nombre','$apellido','$correo')";
+            $sql = "INSERT INTO clientes (Cedula, nombre, apellido, correo, Telefono) VALUES ('$cedula','$nombre','$apellido','$correo','$telefono' )";
            
 
             $query = $con->query($sql);

4330326c80841563192c577f8ec835fd7d37497b
SkyBuilders32 <skybuilders32@gmail.com>
2023-07-10 14:34:12 -0500

    asd

--- DEFINITIVA/PHP/eliminar_user.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $id= $_GET['id'];
-    $conexion=mysqli_connect("localhost","root","","megacom");
-    $consulta= mysqli_query($conexion,"DELETE FROM clientes WHERE Cedula= '$cedula'");
-
-    header('Location: mclientes.php');

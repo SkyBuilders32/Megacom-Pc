69057a54e729993f2e8e793d67043935cb855e8e
UsmeGallego101206 <jfusme@outlook.es>
2023-07-12 07:28:54 -0500

    j

--- DEFINITIVA/PHP/Conexion.php
+++
@@ -1,5 +0,0 @@
-<?php
-$con = new mysqli("localhost", "root", "", "megacom");
-if ($con->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
-}

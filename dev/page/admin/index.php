92b3e452e7758d0e5e9d76fbaccfb2fb2c0d5587
SkyBuilders32 <skybuilders32@gmail.com>
2023-07-24 15:03:19 -0500

    ssss

--- dev/page/admin/index.php
+++ dev/page/admin/index.php
@@ -43,11 +43,6 @@ if ($rol == 2) {
 		<a href="" class="brand"><i class='bx bxs-smile icon'></i> AdminSite</a>
 		<ul class="side-menu">
 			<li><a href="#" class="active"><i class='bx bxs-dashboard icon' ></i> Panel</a></li>
-			<li class="divider" data-text="Control De Tablas"></li>
-			<li><a href="productos/index.php"><i class='bx bxs-inbox icon' ></i> Productos</i></a></li>
-			<li><a href="clientes/index.php"><i class='bx bxs-widget icon' ></i>Clientes</a></li>
-			<li class="divider" data-text="Inventario4">Table and forms</li>
-			<li><a href="#"><i class='bx bx-table icon' ></i> Tables</a></li>
 			<li class="divider" data-text="main">Main</li>
 			<li>
 				<a href="#"><i class='bx bxs-inbox icon' ></i> Productos <i class='bx bx-chevron-right icon-right' ></i></a>

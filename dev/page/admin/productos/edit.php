b2ede2b57bd22a3052789751e2e64761b0790426
usme <jfusme@outlook.es>
2023-08-16 21:26:55 -0500

    edit

--- dev/page/admin/productos/edit.php
+++ dev/page/admin/productos/edit.php
@@ -4,10 +4,10 @@
     include '../../../conexion/conexion.php';
  
     if(isset($_POST['edit'])){
-            //$nombre=$_FILES['imagen']['tmp_name'];//nombre temporal
-            //$truta='imagenes/'.$_FILES['imagen']['name']; // esta ruta según la ubicación de tus archivos
-            //move_uploaded_file($nombre,$truta);//manda la imagen a la carpeta del proyecto
-            $imagen = addslashes(file_get_contents($_FILES ['imagen'] ['tmp_name'] ));
+            $nombre=$_FILES['imagen']['tmp_name'];//nombre temporal
+            $truta='imagenes/'.$_FILES['imagen']['name']; // esta ruta según la ubicación de tus archivos
+            move_uploaded_file($nombre,$truta);//manda la imagen a la carpeta del proyecto
+            //$imagen = addslashes(file_get_contents($_FILES ['imagen'] ['tmp_name'] ));
             $id_producto = $_GET['id_producto'];
             $marca = $_POST['marca'];
             $modelo = $_POST['modelo'];
@@ -17,7 +17,7 @@
             $proveedor = $_POST['proveedor'];
             
  
-            $upd = "UPDATE productos set imagen='$imagen',  marca='$marca', modelo='$modelo', precio_base='$precio_base', existencias='$existencias', descripcion='$descripcion', proveedor='$proveedor' WHERE id_producto like $id_producto";
+            $upd = "UPDATE productos set imagen='$truta',  marca='$marca', modelo='$modelo', precio_base='$precio_base', existencias='$existencias', descripcion='$descripcion', proveedor='$proveedor' WHERE id_producto like $id_producto";
 
             $query = mysqli_query($con, $upd);
  

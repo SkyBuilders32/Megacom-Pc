ced036b829e645c61da128f08941c63d5f0944e6
usme <jfusme@outlook.es>
2023-09-11 16:11:01 -0500

    Update index.php

--- dev/page/admin/ventas/index.php
+++ dev/page/admin/ventas/index.php
@@ -29,11 +29,13 @@ if ($rol == 2) {
 <html lang="en">
 <head>
 <meta charset="UTF-8">
+<?php include "includes/scripts.php" ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="../assets/css/style.css">
 
-    <title>AdminSite</title>
+    <title>Nueva Venta</title>
 </head>
 <body>
     <!-- SIDEBAR -->
@@ -94,7 +96,45 @@ if ($rol == 2) {
             </div>
         </nav>
         <!-- NAVBAR -->
-        
+        <section id="container"> 
+            <div class="title_page">
+                <h1> <i class="fas fa-cube"></i>Nueva Venta </h1>
+            </div>
+            <div class="datos_cliente">
+                <div class="action_cliente">
+                    <h4>Datos del cliente</h4>
+                    <a href="" class="btn_new btn_new_cliente"> <i class="fas fa-plus"></i>Nuevo Cliente</a>
+                </div>
+
+                <form name="form_new_cliente_venta" id="form_new_cliente_venta" class="datos">
+                    <input type="hidden" name="action" value="addcliente">
+                    <input type="hidden" id="idcliente" name="idcliente" value="" required>
+
+                    <div class="wd30">
+                        <label>Nit</label>
+                        <input type="text" name="nit_cliente" id="nit_cliente">
+                    </div>
+
+                    <div class="wd30">
+                        <label>Nombre</label>
+                        <input type="text" name="nom_cliente" id="nom_cliente" disabled required>
+                    </div>
+
+                    <div class="wd30">
+                        <label>Telefono</label>
+                        <input type="number" name="tel_cliente" id="tel_cliente" disabled required>
+                    </div>
+
+                    <div class="wd100">
+                        <label>Direccion</label>
+                        <input type="text" name="dir_cliente" id="dir_cliente" disabled required>
+                    </div>
+                    <div id="div_registro_cliente" class="wd100"> 
+                        <button type="submit" class="btn_save"> <i class="far fa-save fa-lg"></i> Guardar </button>
+                    </div>
+                </form>
+            </div>
+        </section>
 
 </body>
 </html>

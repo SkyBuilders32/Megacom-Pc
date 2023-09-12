ccdb8da4a49d11696b83dc3d776c73e943a83c4b
usme <jfusme@outlook.es>
2023-09-12 08:34:45 -0500

    Update index.php

--- page/admin/ventas/index.php
+++ page/admin/ventas/index.php
@@ -34,6 +34,9 @@ if ($rol == 2) {
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="../assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> 
 
     <title>Nueva Venta</title>
 </head>
@@ -79,55 +82,7 @@ if ($rol == 2) {
             </div>
         </nav>
         <!-- NAVBAR -->
-        <section id="container"> 
-            <div class="title_page">
-                <h1> <i class="fas fa-cube"></i>Nueva Venta </h1>
-            </div>
-            <div class="datos_cliente">
-                <div class="action_cliente">
-                    <h4>Datos del cliente</h4>
-                    <a href="" class="btn_new btn_new_cliente"> <i class="fas fa-plus"></i>Nuevo Cliente</a>
-                </div>
-
-                <form name="form_new_cliente_venta" id="form_new_cliente_venta" class="datos">
-                    <input type="hidden" name="action" value="addcliente">
-                    <input type="hidden" id="idcliente" name="idcliente" value="" required>
-
-                    <div class="wd30">
-                        <label>Nit</label>
-                        <input type="text" name="nit_cliente" id="nit_cliente">
-                    </div>
-
-                    <div class="wd30">
-                        <label>Nombre</label>
-                        <input type="text" name="nom_cliente" id="nom_cliente" disabled required>
-                    </div>
-
-                    <div class="wd30">
-                        <label>Telefono</label>
-                        <input type="number" name="tel_cliente" id="tel_cliente" disabled required>
-                    </div>
-
-                    <div class="wd100">
-                        <label>Direccion</label>
-                        <input type="text" name="dir_cliente" id="dir_cliente" disabled required>
-                    </div>
-                    <div id="div_registro_cliente" class="wd100"> 
-                        <button type="submit" class="btn_save"> <i class="far fa-save fa-lg"></i> Guardar </button>
-                    </div>
-                </form>
-            </div>
-            <div class="datos_venta"></div>
-            <div class="datos">
-                <div class="wd50">
-                    <label >Vendedor</label>
-                    <p>*T*T*T*T*T*TT*</p>
-                </div>
-            </div>
-        </section>
-
-
-
+        
         <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
         <script src="../assets/js/app.js"></script>
 </body>
